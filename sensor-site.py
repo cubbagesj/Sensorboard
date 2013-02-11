cbe90915ed24b170f2cc042279d4b55d14ecd7b4
Sam <sam@CUB2.(none)>
2013-02-11 18:50:27 -0500

    Now gets the last readings from the sensordb and displays them

--- sensor-site.py
+++ sensor-site.py
@@ -1,17 +1,36 @@
 from flask import Flask, render_template
 import datetime
+
+import sqlite3 as lite
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    now = datetime.datetime.now()
-    timeString = now.strftime("%Y-%m-%d %H:%M")
-    templateData = {
-       'title': 'HELLO!',
-       'time': timeString
-       }
+def current():
+    con = lite.connect('sensordb')
+    with con:
+        con.row_factory = lite.Row
+        cur = con.cursor()
+        cur.execute("SELECT * FROM readings WHERE ID = (SELECT MAX(ID) FROM readings)")
+
+        row = cur.fetchone()
+
+        templateData = {
+           'date': row["date"],
+           'time': row["time"],
+           'outside': row["Outside"],
+           'masterbr': row["MasterBR"],
+           'waterhtr': row["WaterHtr"],
+           'basement': row["Basement"],
+           'humidity': row["Humidity"],
+           'furn_out': row["Furn_Out"],
+           'furn_in' : row["Furn_In"],
+           'onboard' : row["Onboard"]
+           }
+    
     return render_template('main.html', **templateData)
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8081, debug=False)
 
